f9125c8494abb3b5c08368a3305f7dfc79a3f2ea
jpgallarte <jpgallarte@gmail.com>
2018-03-05 21:41:09 +0800

    updated db migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,31 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert(
+            array(
+                'id'  => 1,
+                'name'  => 'librarian' , 
+                'email'  => 'librarian@riolibrary.com', 
+                'password'  => '$2y$10$k1bF4k6G8.10hfOUJa4ZkuywuEFK7piRWQDhrYSg9KgiGMHsYan22', 
+                'usertype'  => '0',
+                'remember_token'  => NULL, 
+                'created_at'  => NULL,
+                'updated_at'  => NULL
+            ),
+            array(
+                'id'  => 2,
+                'name'  => 'bookworm' , 
+                'email'  => 'bookworm@riolibrary.com', 
+                'password'  => '$2y$10$J/kXKfyX0vjc/sqJJvcoUuitGlJOrXTVqavG8r1rPrAgJVCLJg7Cu', 
+                'usertype'  => '1',
+                'remember_token'  => NULL, 
+                'created_at'  => NULL,
+                'updated_at'  => NULL
+            )
+        );
+
+        DB::statement("ALTER TABLE books AUTO_INCREMENT = 3;");
     }
 
     /**
